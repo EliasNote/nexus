e2970c9199b26728e02bdaab8de2f7adc7ec16d5
EliasNote <elias.coder1@gmail.com>
2026-04-25 00:29:28 -0300

    Aualização para botões e estilização

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,18 @@ function App() {
         classname="fixed inset-0 z-0 pointer-events-none"
       />
       <main className="relative z-10 flex min-h-screen items-center justify-center">
-        <h1 className="text-4xl font-bold text-blue-500">
-          Olá React + Tailwind v4!
-        </h1>
+        <div className="flex gap-4 items-center justify-center font-normal text-sm">
+              <button
+                className="px-7 py-2.5 bg-[#2563EB] border font-bold border-[#5C8FFF] text-white shadow-lg cursor-pointer"
+              >
+                Começar
+              </button>
+              <button
+                className="px-7 py-2.5 bg-zinc-800 border font-bold border-zinc-600 text-white shadow-lg cursor-pointer"
+              >
+                Como usar
+              </button>
+            </div>
       </main>
     </div>
   )
