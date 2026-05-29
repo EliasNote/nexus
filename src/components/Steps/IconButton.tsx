0a83afc0c2fb2104005a3fa5f422020ae22ac87c
EliasNote <elias.coder1@gmail.com>
2026-05-28 23:13:15 -0300

    remove: Remoção de código não usado

--- src/components/Steps/IconButton.tsx
+++ src/components/Steps/IconButton.tsx
@@ -28,24 +28,3 @@ export const IconButton = ({
     </button>
   );
 };
-
-// <button className="flex p-[14px] justify-start items-center w-full h-[63px] bg-zinc-900 border border-zinc-800">
-//   <div className="flex flex-row gap-[14px] justify-center items-start">
-//     <div className="flex flex-row justify-center items-center border border-zinc-700 w-[35px] h-[35px] bg-zinc-950">
-//       <FileUp
-//         width={22}
-//         height={22}
-//         strokeWidth={1.5}
-//         className="text-zinc-400"
-//       />
-//     </div>
-//     <div className="flex flex-col items-start">
-//       <span className="text-white text-[14px]">
-//         Selecionar arquivo
-//       </span>
-//       <span className="text-zinc-600 text-[12px]">
-//         Carregar cofre existente
-//       </span>
-//     </div>
-//   </div>
-// </button>
