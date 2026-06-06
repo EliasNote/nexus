fd18254dbe1dc5d63893576295ebe2783bd76f84
EliasNote <elias.coder1@gmail.com>
2026-06-06 15:49:31 -0300

    feat

--- front/src/pages/Dashboard/components/Credential/Credential.tsx
+++ front/src/pages/Dashboard/components/Credential/Credential.tsx
@@ -3,10 +3,10 @@ import { Login } from "./Login";
 
 export const Credential = ({ credential }: { credential?: VaultEntry }) => {
   return (
-    <section className="flex w-full px-[40px]">
+    <section className="flex w-full">
       {credential ? (
         <>
-          <div className="flex flex-row w-full py-[30px] gap-[10px]">
+          <div className="flex flex-row w-full h-fit px-[40px] py-[30px] gap-[10px] border-b border-zinc-800">
             <div className="w-[60px] h-[60px] flex items-center justify-center text-[32px] bg-zinc-900 border border-zinc-800 text-zinc-300">
               {credential?.title[0].toUpperCase()}
             </div>
