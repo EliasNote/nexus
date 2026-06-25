28efd4c5b9f4159f4e4767415715e543a6f449ed
EliasNote <elias.coder1@gmail.com>
2026-06-25 00:23:58 -0300

    refactor: Remoção da memória persistente

--- front/src/hooks/useCloudStore.ts
+++ front/src/hooks/useCloudStore.ts
@@ -1,5 +1,4 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
 import { wrap } from "comlink";
 import type { CryptoService } from "../utils/worker";
 import type { EncryptedVault, VaultSummarizedData } from "@/types/vault";
@@ -38,57 +37,50 @@ type CloudState = {
   setAutoSaveInterval: (interval: number) => void;
 };
 
-export const useCloudStore = create<CloudState>()(
-  persist(
-    (set) => ({
-      vault: null,
-      setVault: (vault) => set({ vault: vault }),
+export const useCloudStore = create<CloudState>()((set) => ({
+  vault: null,
+  setVault: (vault) => set({ vault: vault }),
 
-      summaryVault: null,
-      setSummaryVault: (summary) => set({ summaryVault: summary }),
+  summaryVault: null,
+  setSummaryVault: (summary) => set({ summaryVault: summary }),
 
+  accessToken: null,
+  setAccessToken: (token) => set({ accessToken: token }),
+
+  refreshToken: null,
+  setRefreshToken: (token) => set({ refreshToken: token }),
+
+  activeProvider: null,
+  setActiveProvider: (provider) => set({ activeProvider: provider }),
+
+  needsRepoFix: false,
+  setNeedsRepoFix: (needsRepoFix) => set({ needsRepoFix }),
+
+  loginRepoNotFound: undefined,
+  setLoginRepoNotFound: (fn) => set({ loginRepoNotFound: fn }),
+
+  clearSession: () => {
+    cryptoService.destroyKey();
+    set({
       accessToken: null,
-      setAccessToken: (token) => set({ accessToken: token }),
-
       refreshToken: null,
-      setRefreshToken: (token) => set({ refreshToken: token }),
-
-      activeProvider: null,
-      setActiveProvider: (provider) => set({ activeProvider: provider }),
-
-      needsRepoFix: false,
-      setNeedsRepoFix: (needsRepoFix) => set({ needsRepoFix }),
-
-      loginRepoNotFound: undefined,
-      setLoginRepoNotFound: (fn) => set({ loginRepoNotFound: fn }),
-
-      clearSession: () => {
-        cryptoService.destroyKey();
-        set({
-          accessToken: null,
-          refreshToken: null,
-          expiresIn: null,
-          isTokenValid: false,
-          vault: null,
-          summaryVault: null,
-          activeProvider: null,
-        });
-      },
-
       expiresIn: null,
-      setExpiresIn: (expiresIn) => set({ expiresIn }),
-
       isTokenValid: false,
-      setIsTokenValid: (isValid) => set({ isTokenValid: isValid }),
+      vault: null,
+      summaryVault: null,
+      activeProvider: null,
+    });
+  },
 
-      isPendingSync: false,
-      setIsPendingSync: (isPendingSync) => set({ isPendingSync }),
+  expiresIn: null,
+  setExpiresIn: (expiresIn) => set({ expiresIn }),
 
-      autoSaveInterval: 1,
-      setAutoSaveInterval: (interval) => set({ autoSaveInterval: interval }),
-    }),
-    {
-      name: "cloud-sync-store",
-    },
-  ),
-);
+  isTokenValid: false,
+  setIsTokenValid: (isValid) => set({ isTokenValid: isValid }),
+
+  isPendingSync: false,
+  setIsPendingSync: (isPendingSync) => set({ isPendingSync }),
+
+  autoSaveInterval: 1,
+  setAutoSaveInterval: (interval) => set({ autoSaveInterval: interval }),
+}));
